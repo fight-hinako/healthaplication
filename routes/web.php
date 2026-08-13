0507a6a5e321fa14e9551d58dc11d07c8ade9ec0
fight-hinako <pinapinahinako21@gmail.com>
2026-08-13 09:39:28 +0900

    authを仲介して、実際にloginしていない人がhome画面等を開けないようにした。 また、すでにloginしている人がlogin画面を開けないようにした。

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,18 @@ use App\Http\Controllers\WorkCountController;
 
 Route::view('/', 'welcome')->name('welcome');
 
+
 Route::get('/createaccount', [CreateAccountController::class, 'create'])->name('createaccount');
 Route::post('/createaccount/submit', [CreateAccountController::class, 'store'])->name('createaccount.submit');
 
-Route::get('/login', [LoginController::class, 'create'])->name('login');
-Route::post('/login/submit', [LoginController::class, 'store'])->name('login.submit');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'create'])->name('login');
+    Route::post('/login/submit', [LoginController::class, 'store'])->name('login.submit');
+});
 
-Route::get('/home', [HomeController::class, 'show'])->name('home');
 
-Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
-Route::post('/dashboard/update', [DashboardController::class, 'update'])->name('dashboard.update');
-
-Route::post('/workcountcard', [WorkCountController::class, 'start'])->name('workcountcard');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'show'])->name('home');
+    Route::get('/dashboard', [DashboardController::class, 'show'])->name('dashboard');
+    Route::post('/dashboard/update', [DashboardController::class, 'update'])->name('dashboard.update');
+});
