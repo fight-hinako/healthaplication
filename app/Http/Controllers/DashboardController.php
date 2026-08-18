ae8ce88856163ce4248c0df0ffe8723b8518e548
fight-hinako <pinapinahinako21@gmail.com>
2026-08-19 07:02:13 +0900

    dashboardのメール・パスワードを変更する欄を有効にしました。

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\View\View;
+use Illuminate\Validation\Rules\Password;
+
 
 class DashboardController extends Controller
 {
@@ -14,16 +16,30 @@ class DashboardController extends Controller
         return view('dashboard');
     }
 
-    public function update(Request $request): RedirectResponse
-    {
-        $request->validate([
-            'countdown_minutes' => ['required', 'integer', Rule::in(array_keys(config('workcountdown.options')))],
-        ]);
+    public function dashboardUpdate(Request $request): RedirectResponse
+   {
+      $validated = $request->validate([
+        'email' => ['nullable', 'email', Rule::unique('users', 'email')->ignore(auth()->id())],
+        'password' => ['nullable', 'required_with:passwordConfirm', 'current_password'],
+        'passwordConfirm' => ['nullable', 'required_with:password', Password::default()],
+        'countdown_minutes' => ['required', 'integer', Rule::in(array_keys(config('workcountdown.options')))],
+]);
 
-        auth()->user()->update([
-            'countdown_minutes' => $request->integer('countdown_minutes'),
-        ]);
+      $user = auth()->user();
+
+      if ($request->filled('email')) {
+          $user->email = $validated['email'];
+       }
+
+       if ($request->filled('passwordConfirm')) {
+          // 今のパスワード確認 + 新パスワード保存
+          $user->password = $validated['passwordConfirm'];
+        }
+
+        $user->countdown_minutes = $request->integer('countdown_minutes');
+        $user->save();
 
         return back()->with('success', '設定を保存しました。');
     }
+
 }
